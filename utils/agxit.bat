b8fd677d33b8c22039cd7e1b05d644b79ec8731c
thomase <thomase@b08dbbb5-5418-0410-9893-8f6b77c4669c>
2006-09-07 22:25:36 +0000

    Removed copy steps, as these will vary depending on local installation and therefore should be in a different script.

--- utils/agxit.bat
+++ utils/agxit.bat
@@ -4,6 +4,5 @@ if (%1)==() exit /b
 
 echo on
 python \Programs\ArchGenXML\Archgenxml.py --author="Sean Gillies" --author="Tom Elliott" --copyright="Ancient World Mapping Center, University of North Carolina at Chapel Hill, U.S.A." --license=GPL -o ./%1 ./models/%1.xmi
-rmdir /s/q \PROGRA~1\PLONE2~1\Data\Products\%1
-xcopy .\%1 \PROGRA~1\PLONE2~1\Data\Products\%1 /E/I
+
 
