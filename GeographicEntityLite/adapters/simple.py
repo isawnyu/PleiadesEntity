13e18dba17277f999597ed0e5d0efb829ccf9c7d
sgillies <sgillies@b08dbbb5-5418-0410-9893-8f6b77c4669c>
2006-09-08 15:59:26 +0000

    adapt GeographicEntityLite to IGeoItemSimple from PleiadesGeocoder

---
+++ GeographicEntityLite/adapters/simple.py
@@ -0,0 +1,109 @@
+# ===========================================================================
+# Copyright (C) 2006 Ancient World Mapping Center
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# 
+# About Pleiades
+# --------------
+#
+# Pleiades is an international research network and associated web portal and
+# content management system devoted to the study of ancient geography. 
+#
+# See http://icon.stoa.org/trac/pleiades/wiki.
+#
+# Funding for the creation of this software was provided by a grant from the 
+# U.S. National Endowment for the Humanities (http://www.neh.gov).
+# ===========================================================================
+
+from zope.interface import implements
+
+from Products.PleiadesGeocoder.interfaces import IGeoItemSimple
+
+class GeoEntitySimple(object):
+    
+    """Python expression of a GeoRSS simple item.
+    """
+    implements(IGeoItemSimple)
+   
+    def __init__(self, context):
+        """Initialize adapter."""
+        self.context = context
+
+    def getSRS(self):
+        return 'EPSG:4326'
+
+    def setSRS(self, srs):
+        pass
+        
+    def getGeometryType(self):
+        return 'point'
+
+    def setGeometryType(self, geomtype):
+        pass
+
+    def getSpatialCoordinates(self):
+        values = [float(v) for v in \
+                    self.context.getSpatialCoordinates().split()]
+        nvalues = len(values)
+        npoints = nvalues/3
+        coords = []
+        for i in range(npoints):
+            coords.append(tuple(values[3*i:3*i+3]))
+        return tuple(coords)
+
+    def setGeometry(self, geomtype, coords):
+        value = ''
+        for point in coords:
+            if len(point) == 3:
+                value = ' '.join([value, "%f %f %f" % point])
+            if len(point) == 2:
+                value = ' '.join([value, "%f %f 0.0" % point])
+            else:
+                raise ValueError, \
+                "Insufficient number of ordinates: %s" % point
+        self.context.setSpatialCoordinates(value.lstrip())
+        
+    def isGeoreferenced(self):
+        """Return True if the object is "on the map"."""
+        return bool(self.context.getSpatialCoordinates())
+        
+    def getInfo(self, dims=3):
+        """Return an informative dict."""
+        context = self.context
+        info = {'srs':                  context.getSRS(),
+                'geometryType':         context.getGeometryType(),
+               }
+        if dims == 3:
+            info['spatialCoordinates'] = context.getSpatialCoordinates()
+        elif dims == 2:
+            values = context.getSpatialCoordinates().split()
+            nvalues = len(values)
+            npoints = nvalues/3
+            coords = []
+            for i in range(npoints):
+                coords.extend(values[3*i:3*i+2])
+            info['spatialCoordinates'] = ' '.join(coords)
+        else:
+            raise ValueError, "Invalid dims: %d" % (dims)
+
+        # Content objects
+        info.update(
+               {'id':           context.getId(),
+                'title':        context.title_or_id(),
+                'description':  context.Description(),
+                'url':          context.absolute_url(),}
+            )
+        return info
+
