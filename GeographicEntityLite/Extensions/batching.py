4786f82506c1e05cddf102dc673ba27e7d98620a
thomase <thomase@b08dbbb5-5418-0410-9893-8f6b77c4669c>
2006-09-12 17:27:43 +0000

    Added setting of description (to "No Description") on names.

--- GeographicEntityLite/Extensions/batching.py
+++ GeographicEntityLite/Extensions/batching.py
@@ -79,6 +79,7 @@ def load_entity(plonefolder, source):
         nameID = en.invokeFactory('GeographicNameLite', id=newEnID + '-n' + `i+1`)
         en_name = getattr(en, nameID)
         en_name.setTitle(nameID)
+        en_name.setDescription('No description')
         en_name.setNameAttested(name.nameString)
         en_name.setNameTransliterated(name.nameStringTransliterated)
         en_name.setNameLanguage(name.language)
