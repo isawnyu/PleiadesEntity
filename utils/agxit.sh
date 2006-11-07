d9afcdfc70dcdfe0c5b1fa85f82a816bee429724
sgillies <sgillies@b08dbbb5-5418-0410-9893-8f6b77c4669c>
2006-11-07 19:05:59 +0000

    update agx script to work with new code structure and add note about how to run

--- utils/agxit.sh
+++ utils/agxit.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-PYTHON="/home/sean/pleiades/plone-sites/p1/bin/python"
-AGXROOT="/home/sean/pleiades/software/ArchGenXML"
-export AGXROOT
-"$PYTHON" "$AGXROOT"/ArchGenXML.py --author="Sean Gillies" --author="Tom Elliott" --copyright="Ancient World Mapping Center, University of North Carolina at Chapel Hill, U.S.A." --license=GPL -o ./GeographicEntityLite ./models/geographicEntityLite.xmi
+#
+# Run this script from the directory above your working copy
+
+PYTHON="/usr/bin/python"
+AGXROOT="/home/sean/Apps/ArchGenXML"
+"$PYTHON" "$AGXROOT"/ArchGenXML.py --author="Sean Gillies" --author="Tom Elliott" --copyright="Ancient World Mapping Center, University of North Carolina at Chapel Hill, U.S.A." --license=GPL -o ./GeographicEntityLite ./GeographicEntityLite/models/geographicEntityLite.xmi
 
