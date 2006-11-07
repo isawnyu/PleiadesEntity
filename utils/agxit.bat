5b4d787062fbf0d19867b8ac4f020ead169bbcd9
thomase <thomase@b08dbbb5-5418-0410-9893-8f6b77c4669c>
2006-11-07 21:17:19 +0000

    Updated to work with new repository (and default checkout) structure.

--- utils/agxit.bat
+++ utils/agxit.bat
@@ -1,5 +1,5 @@
 set PYTHON=c:\Python24\python
 set AGXROOT=c:\Programs\ArchGenXML
-%PYTHON% %AGXROOT%\Archgenxml.py --author="Sean Gillies" --author="Tom Elliott" --copyright="Ancient World Mapping Center, University of North Carolina at Chapel Hill, U.S.A." --license=GPL -o ./GeographicEntityLite ./models/GeographicEntityLite.xmi
+%PYTHON% %AGXROOT%\Archgenxml.py --author="Sean Gillies" --author="Tom Elliott" --copyright="Ancient World Mapping Center, University of North Carolina at Chapel Hill, U.S.A." --license=GPL -o ./GeographicEntityLite ./GeographicEntityLite/models/GeographicEntityLite.xmi
 
 
