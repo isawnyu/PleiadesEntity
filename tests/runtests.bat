e187031f62822741ec658dbc48893abb4a5c9168
thomase <thomase@b08dbbb5-5418-0410-9893-8f6b77c4669c>
2006-11-17 12:34:53 +0000

    Modified to work with 2.5.1/Zope 2.9.4. See: http://plone.org/documentation/error/attributeerror-test_user_1_ and http://comments.gmane.org/gmane.comp.web.zope.plone.devel/13119

--- tests/runtests.bat
+++ tests/runtests.bat
@@ -1,10 +1,9 @@
-@set PYTHON=C:\Program Files\Plone 2\Python\python.exe
 @set ZOPE_HOME=C:\Program Files\Plone 2\Zope
-@set INSTANCE_HOME=C:\Program Files\Plone 2\Data
+@set PYTHON=C:\Program Files\Plone 2\Python\python.exe
 @set SOFTWARE_HOME=C:\Program Files\Plone 2\Zope\lib\python
+@set INSTANCE_HOME=%SOFTWARE_HOME%
 @set CONFIG_FILE=C:\Program Files\Plone 2\Data\etc\zope.conf
 @set PYTHONPATH=%SOFTWARE_HOME%
-@set ZOPE_RUN=C:\Program Files\Plone 2\Zope\lib\python\Zope2\Startup\run.py
-@set PATH=%PATH%;C:\Program Files\Plone 2\Python
-"%PYTHON%" runalltests.py
+@set TEST_RUN=%ZOPE_HOME%\bin\test.py
 
+"%PYTHON%" "%TEST_RUN%" --usecompiled -vp --config-file="%CONFIG_FILE%" -s Products.PleiadesEntity  
