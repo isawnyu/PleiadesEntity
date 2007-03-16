e2cef89901f2c45ef90ef459c5b904c38ac4ccc7
sgillies <sgillies@b08dbbb5-5418-0410-9893-8f6b77c4669c>
2007-03-16 20:47:42 +0000

    Additions to the profile: scripts under /places, front pages, and skin customizations (#18)

---
+++ profiles/default/structure/places/search.py
@@ -0,0 +1,13 @@
+## Script (Python) "search"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=q
+##title=
+##
+pid = container.getBaIdent(q)
+response = container.REQUEST.RESPONSE
+response.setStatus(301)
+response.redirect("http://pleiades.stoa.org/places/%s" % pid)
