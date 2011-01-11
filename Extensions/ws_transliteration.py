5cfbcf07afcf2fdcca94719f807eb923dd0ffbd1
sgillies <sgillies@b08dbbb5-5418-0410-9893-8f6b77c4669c>
2011-01-11 18:48:12 +0000

    Import all transliteration.

--- Extensions/ws_transliteration.py
+++ Extensions/ws_transliteration.py
@@ -1,13 +1,2 @@
-from pleiades.transliteration import ws_grek, ws_latn
+from pleiades.transliteration import transliterate_name, ws_grek, ws_latn
 
-def transliterate_name(lang, name_utf8):
-    wsystem = lang.lower()
-    name = unicode(name_utf8, 'utf-8')
-    if wsystem == 'grc' or wsystem == 'la-grek':
-        transliteration = ws_grek.transliterate(name)
-    elif wsystem == 'la' or wsystem == 'grc-latn':
-        transliteration = ws_latn.transliterate(name)
-    else:
-        return 'Unsupported writing system (%s) in PleiadesEntity/Extensions/ws_transliteration.py' % lang
-    return transliteration
-    
