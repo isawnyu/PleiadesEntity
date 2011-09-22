d19ae8efea5455a62d3c65554da58cb6f2278579
sgillies <sgillies@b08dbbb5-5418-0410-9893-8f6b77c4669c>
2011-09-22 20:03:25 +0000

    Index first character of place titles

--- indexers.py
+++ indexers.py
@@ -1,10 +1,15 @@
 from plone.indexer.decorator import indexer
 
-from Products.PleiadesEntity.content.interfaces import IName
+from Products.PleiadesEntity.content.interfaces import IName, IPlace
 
 
 @indexer(IName)
 def name_titleStarts(object, **kw):
     tvalue = object.getNameTransliterated() or object.Title() or "?"
+    return tvalue[0].upper()
+
+@indexer(IPlace)
+def place_titleStarts(object, **kw):
+    tvalue = object.Title() or "?"
     return tvalue[0].upper()
 
