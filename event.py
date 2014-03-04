84de0b15cf93528ab3c930aabb9216f806ebba56
Tom Elliott <tom.elliott@nyu.edu>
2014-03-04 07:38:49 -0500

    test import of geojson and wrap function from pleiades.json

--- event.py
+++ event.py
@@ -1,3 +1,4 @@
+import geojson
 import logging
 
 from Acquisition import aq_inner, aq_parent
@@ -11,6 +12,7 @@ from Products.PleiadesEntity.content.interfaces import ILocation, IName
 from Products.PleiadesEntity.content.interfaces import IFeature, IPlace
 from Products.PleiadesEntity.time import temporal_overlap
 from pleiades.transliteration import transliterate_name
+from pleiades.json.browser import wrap 
 
 log = logging.getLogger('PleiadesEntity')
 
