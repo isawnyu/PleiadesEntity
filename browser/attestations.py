eba8a98e4141d9e33f7abcf751c3fd1343dd09d7
Tom Elliott <tom.elliott@nyu.edu>
2014-03-12 12:37:40 -0500

    produce integer on list index test

--- browser/attestations.py
+++ browser/attestations.py
@@ -141,7 +141,7 @@ class NamesTable(ChildrenTable):
     def postfix(self, ob):
         acert = ob.getAssociationCertainty();
         nameAttested = ob.getNameAttested() or None
-        nameTransliterated = [u'', ob.Title][nameAttested]
+        nameTransliterated = [u'', ob.Title][nameAttested is not None]
         pfixs = [u'\%s', u'(%s) \%s' % nameTransliterated][nameTransliterated != u'']
         if acert == 'less-certain':
             return pfixs % u'?'
