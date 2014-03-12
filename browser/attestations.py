d73e6c6cfb89ccfa98d61b9d5a28945cb56d545e
Tom Elliott <tom.elliott@nyu.edu>
2014-03-12 18:40:32 -0500

    sort names

--- browser/attestations.py
+++ browser/attestations.py
@@ -174,7 +174,7 @@ class NamesTable(ChildrenTable):
         vocab = self.vtool.getVocabularyByName('ancient-name-languages')
         self.langs = dict(vocab.getDisplayList(vocab).items())
         output = []
-        for score, ob, nrefs in sorted(names, reverse=False):
+        for score, ob, nrefs in sorted(names, key=lamba title: ob.Title(), reverse=False):
             nameAttested = ob.getNameAttested() or None
             title = ob.Title() or "Untitled"
             if nameAttested:
