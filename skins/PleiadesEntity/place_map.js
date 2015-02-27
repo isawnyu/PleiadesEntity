5c401a638fe1a21b68410a646b2c101206a6ceed
Tom Elliott <tom.elliott@nyu.edu>
2015-02-27 11:58:40 -0600

    tweak icon popup anchors

--- skins/PleiadesEntity/place_map.js
+++ skins/PleiadesEntity/place_map.js
@@ -133,10 +133,10 @@ var target = null;
 /* set up icons for vector layers */
 
 var connectionIcon = new L.Icon({
-    iconUrl: "http://pleiades.stoa.org/images/pmapi/21/connection-green.png",
+    iconUrl: "http://pleiades.stoa.org/images/pmapi/21/connection-blue.png",
     iconSize:     [21, 26],
     iconAnchor:   [12, 28],
-    popupAnchor:  [0, -30]
+    popupAnchor:  [0, -26]
   });
 
 var locationIcon = new L.Icon({
@@ -147,10 +147,10 @@ var locationIcon = new L.Icon({
   });
 
 var baselineLocationIcon = new L.Icon({
-    iconUrl: "http://pleiades.stoa.org/images/pmapi/32/location-brown.png",
-    iconSize:     [32, 37],
-    iconAnchor:   [15, 35],
-    popupAnchor:  [0, -32]
+    iconUrl: "http://pleiades.stoa.org/images/pmapi/21/location-green.png",
+    iconSize:     [21, 26],
+    iconAnchor:   [12, 28],
+    popupAnchor:  [0, -26]
   });
 
 
