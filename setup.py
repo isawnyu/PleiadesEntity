d6da74c0fb8adbbd2a5be3ac49e013cd62e1e268
David Glick <david@glicksoftware.com>
2016-02-08 16:07:25 -0800

    turn PleiadesEntity into an egg

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+version = '0.5'
+
+setup(
+    name='Products.PleiadesEntity',
+    version=version,
+    description="",
+    long_description="""\
+""",
+    classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='',
+    author='Institute for the Study of the Ancient World',
+    author_email='isaw@nyu.edu',
+    url='https://github.com/isawnyu/PleiadesEntity',
+    license='GPL',
+    packages=find_packages(),
+    namespace_packages=['Products'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+    extras_require={
+        'test': ['Products.PloneTestCase'],
+    },
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+)
