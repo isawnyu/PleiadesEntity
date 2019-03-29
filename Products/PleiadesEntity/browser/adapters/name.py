0f0163d94314f550baf0247a71726421c85eb4a9
Matthew Wilkes <git@matthewwilkes.name>
2019-03-29 16:46:13 +0000

    Add URI to various vocabulary JSON fields
    A number of JSON fields contain the value of a vocabulary. This change
    adds a field alongside (usually fieldURI) that contains the fully-qualified
    URI to the vocabulary item.

--- Products/PleiadesEntity/browser/adapters/name.py
+++ Products/PleiadesEntity/browser/adapters/name.py
@@ -2,6 +2,7 @@ from . import ContentExportAdapter
 from . import PlaceSubObjectExportAdapter
 from . import TemporalExportAdapter
 from . import WorkExportAdapter
+from . import CertaintyExportAdapter
 from . import archetypes_getter
 from . import export_config
 from . import memoize_all_methods
@@ -10,7 +11,7 @@ from . import memoize_all_methods
 @memoize_all_methods
 class NameExportAdapter(
         WorkExportAdapter, TemporalExportAdapter, ContentExportAdapter,
-        PlaceSubObjectExportAdapter):
+        CertaintyExportAdapter, PlaceSubObjectExportAdapter):
 
     attested = archetypes_getter('nameAttested')
     language = archetypes_getter('nameLanguage')
@@ -18,7 +19,6 @@ class NameExportAdapter(
     nameType = archetypes_getter('nameType')
     transcriptionAccuracy = archetypes_getter('accuracy')
     transcriptionCompleteness = archetypes_getter('completeness')
-    associationCertainty = archetypes_getter('associationCertainty')
     details = archetypes_getter('text')
 
 
