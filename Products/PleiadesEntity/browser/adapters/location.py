71220bbf1e00b9d5410e6532fe121b9f669368b4
Tom Elliott <tom.elliott@nyu.edu>
2020-07-02 16:48:54 -0500

    use getField

--- Products/PleiadesEntity/browser/adapters/location.py
+++ Products/PleiadesEntity/browser/adapters/location.py
@@ -49,7 +49,7 @@ class LocationExportAdapter(
             accuracy_path = '/'.join(accuracy.getPhysicalPath())
             portal = api.portal.get()
             my_accuracy = portal.restrictedTraverse(accuracy_path)
-            v = my_accuracy.get('value')
+            v = my_accuracy.getField('value').get(inst)
             return v
             
 
