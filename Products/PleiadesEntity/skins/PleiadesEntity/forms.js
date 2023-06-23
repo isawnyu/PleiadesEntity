97a43faf8c4202e76ff9b5ba25a97a2a176dc08f
Silvio Tomatis <silviot@gmail.com>
2023-06-23 14:59:48 +0200

    Do not use jQuery.live: use jQuery.on instead

--- Products/PleiadesEntity/skins/PleiadesEntity/forms.js
+++ Products/PleiadesEntity/skins/PleiadesEntity/forms.js
@@ -67,91 +67,106 @@ jQuery(function () {
       'input.context[value="Add reference"]',
       enable_zotero
   );
-  $('.short-title-wrapper input').live('keyup', function (ev) {
-    var $this = $(this);
-    var currentValue = $this.val();
-    $button = $this.parent().next();
-    if (currentValue == "") {
-      $button.css("opacity", "0.5");
-    } else {
-      $button.css("opacity", "1");
-    }
+  $('#archetypes-fieldname-referenceCitations').on(
+    'keyup',
+    '.short-title-wrapper input',
+    function (ev) {
+      var currentValue = ev.target.value;
+      // Since autocomplete added its structure we need to climb up the DOM twice
+      $button = $(ev.target).parent().parent().next();
+      if (currentValue == "") {
+        $button.css("opacity", "0.5");
+      } else {
+        $button.css("opacity", "1");
+        // check if the key pressed was enter
+        if (ev.keyCode == 13) {
+          $button.click();
+        }
+      }
   });
 
   const setIcon = (button, icon) => {
     button.text(button.text().trim().replace(/^[^ ]* /, icon + ' '));
   }
 
-  $('.short-title-wrapper .copy-zotero-uri').live('click', function (ev) {
-    ev.preventDefault();
-    $this = $(this);
-    var $inputField = $this.parent("div.short-title-wrapper").find("input");
-    var $resultDiv = $this.parent("div.short-title-wrapper").find(".zotero-api-result");
-    var $bibliographicURIField = $this.closest("fieldset").find('input[id$="bibliographic_uri"]');
-    var currentValue = $inputField.val();
-    if (currentValue && ! $this.attr("data-fetching")) {
-      var url = 'https://api.zotero.org/groups/2533/items/top?limit=10&itemType=-attachment&q=' + encodeURIComponent(currentValue);
-      // Send a request to the backend using the fetch api, and log the result to the console
-      setIcon($this, "⏳");
-      $this.css("opacity", "0.5");
-      $this.attr("data-fetching", "true");
-      $resultDiv.text("");
-      fetch(url).then(function(response) {
-        return response.json();
-      }).then(function(data) {
-        $this.attr("data-fetching", "")
-        $this.css("opacity", "1");
-        setIcon($this, "🔽");
-        if (data.length == 1) {
-          $inputField.val(data[0].data.shortTitle);
-          $bibliographicURIField.val(data[0].links.alternate.href);
-          $resultDiv.css("background-color", "white");
-        } else if (data.length == 0) {
-          $resultDiv.text("No results found searching for " + currentValue);
-          $resultDiv.css("background-color", "lightpink");
-        } else {
-          $resultDiv.html("Results found for <i>" + currentValue + "</i>:");
-          $resultDiv.css("background-color", "lightgrey");
-          // Loop over data
-          data.forEach(function(item) {
-            var item_url = 'https://www.zotero.org/groups/pleiades/items/' + item.key;
-            var link_text = '<a data-short-title="' + item.data.shortTitle + '" class="zotero-choice zotero-item-' + item.key;
-            link_text += '" href="' + item_url + '">⏳'+ (item.data.shortTitle || item.data.title) + '</a>'
-            $resultDiv[0].innerHTML += '<br />' + link_text;
-            $.getJSON(
-              fetch_url,
-              { "url": item_url },
-              function (data) {
-                if (data.error) { $(".zotero-item-" + item.key).css("color", "red").attr("data-invalid", "true"); return }
-                $(".zotero-item-" + item.key).html(data.formatted_citation);
-              }).fail(function () {
-                markError($(".zotero-item-" + item.key))
-            });
+  $('#archetypes-fieldname-referenceCitations').on(
+    'click',
+    '.short-title-wrapper .copy-zotero-uri',
+    function (ev) {
+      ev.preventDefault();
+      $this = $(this);
+      var $inputField = $this.parent("div.short-title-wrapper").find("input");
+      var $resultDiv = $this.parent("div.short-title-wrapper").find(".zotero-api-result");
+      var $bibliographicURIField = $this.closest("fieldset").find('input[id$="bibliographic_uri"]');
+      var currentValue = $inputField.val();
+      if (currentValue && ! $this.attr("data-fetching")) {
+        var url = 'https://api.zotero.org/groups/2533/items/top?limit=10&itemType=-attachment&q=' + encodeURIComponent(currentValue);
+        // Send a request to the backend using the fetch api, and log the result to the console
+        setIcon($this, "⏳");
+        $this.css("opacity", "0.5");
+        $this.attr("data-fetching", "true");
+        $resultDiv.text("");
+        fetch(url).then(function(response) {
+          return response.json();
+        }).then(function(data) {
+          $this.attr("data-fetching", "")
+          $this.css("opacity", "1");
+          setIcon($this, "🔽");
+          if (data.length == 1) {
+            $inputField.val(data[0].data.shortTitle);
+            $bibliographicURIField.val(data[0].links.alternate.href);
+            $resultDiv.css("background-color", "white");
+          } else if (data.length == 0) {
+            $resultDiv.text("No results found searching for " + currentValue);
+            $resultDiv.css("background-color", "lightpink");
+          } else {
+            $resultDiv.html("Results found for <i>" + currentValue + "</i>:");
+            $resultDiv.css("background-color", "lightgrey");
+            // Loop over data
+            data.forEach(function(item) {
+              var item_url = 'https://www.zotero.org/groups/pleiades/items/' + item.key;
+              var link_text = '<a data-short-title="' + item.data.shortTitle + '" class="zotero-choice zotero-item-' + item.key;
+              link_text += '" href="' + item_url + '">⏳'+ (item.data.shortTitle || item.data.title) + '</a>'
+              $resultDiv[0].innerHTML += '<br />' + link_text;
+              $.getJSON(
+                fetch_url,
+                { "url": item_url },
+                function (data) {
+                  if (data.error) { $(".zotero-item-" + item.key).css("color", "red").attr("data-invalid", "true"); return }
+                  $(".zotero-item-" + item.key).html(data.formatted_citation);
+                }).fail(function () {
+                  markError($(".zotero-item-" + item.key))
+              });
+          });
+        }
+      }).catch(function(err) {
+          console.log('Fetch Error :-S', err);
         });
       }
-    }).catch(function(err) {
-        console.log('Fetch Error :-S', err);
-      });
+      return false;
     }
-    return false;
-  });
+  );
   function markError($link) {
     $link.css("color", "red").attr("data-invalid", "true");
     var body = $link.html();
     $link.html(body.replace(/⏳/g, "⚠️"));
   }
-  $("a.zotero-choice").live("click", function (ev) {
-    ev.preventDefault();
-    var $this = $(this);
-    if ($this.attr("data-invalid")) { return false; }
-    var $bibliographicURIField = $this.closest("fieldset").find('input[id$="bibliographic_uri"]');
-    $bibliographicURIField.val($this.attr("href"));
-    var $formattedCitationField = $this.closest("fieldset").find('input[id$="formatted_citation"]');
-    $formattedCitationField.val($this.html());
-    var $shortTitleField = $this.closest("div.short-title-wrapper").find("input");
-    $shortTitleField.val($this.attr("data-short-title"));
-    return false;
-  });
+  $('#archetypes-fieldname-referenceCitations').on(
+    "click",
+    "a.zotero-choice",
+    function (ev) {
+      ev.preventDefault();
+      var $this = $(this);
+      if ($this.attr("data-invalid")) { return false; }
+      var $bibliographicURIField = $this.closest("fieldset").find('input[id$="bibliographic_uri"]');
+      $bibliographicURIField.val($this.attr("href"));
+      var $formattedCitationField = $this.closest("fieldset").find('input[id$="formatted_citation"]');
+      $formattedCitationField.val($this.html());
+      var $shortTitleField = $this.closest("div.short-title-wrapper").find("input");
+      $shortTitleField.val($this.attr("data-short-title"));
+      return false;
+    }
+  );
 
   // Autocomplete for Short Title
   const default_works = Object.keys(pleiades_default_works);
